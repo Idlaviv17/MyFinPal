0c1cc95ad0b0c9a926aea39ddeef6a0d499147a9
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-04-07 17:49:54 -0700

    New Record Fragment

--- app/src/main/java/angulo/javier/myfinpal/ui/new_record/NewRecordFragment.kt
+++ app/src/main/java/angulo/javier/myfinpal/ui/new_record/NewRecordFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import angulo.javier.myfinpal.R
 import angulo.javier.myfinpal.databinding.FragmentNewRecordBinding
 
 class NewRecordFragment : Fragment() {
 
     private var _binding: FragmentNewRecordBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,11 +27,25 @@ class NewRecordFragment : Fragment() {
         _binding = FragmentNewRecordBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupSpinner(binding.establishmentsSpinner, R.array.establishments)
+        setupSpinner(binding.paymentMethodsSpinner, R.array.payment_methods)
+        setupSpinner(binding.categoriesSpinner, R.array.categories)
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupSpinner(spinner: Spinner, itemsArrayResourceId: Int) {
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            itemsArrayResourceId,
+            R.layout.spinner_item_text
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
