9fc74b24986ded0f4b039688df9f68bcb22eddfb
Joshiva Yzair <joshivatapia@gmail.com>
2024-04-18 23:50:32 -0600

    Edit Budget View Created

--- app/src/main/java/angulo/javier/myfinpal/ui/budget/BudgetFragment.kt
+++ app/src/main/java/angulo/javier/myfinpal/ui/budget/BudgetFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import angulo.javier.myfinpal.R
 import angulo.javier.myfinpal.databinding.FragmentBudgetBinding
 
 class BudgetFragment : Fragment() {
@@ -27,11 +29,25 @@ class BudgetFragment : Fragment() {
         _binding = FragmentBudgetBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
+        // Configura el OnClickListener para el botón
+        binding.btnEditBudget.setOnClickListener {
+            // Navega a otro fragmento (ejemplo: a un fragmento llamado EditBudgetFragment)
+            navigateToEditBudget()
+        }
+
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun navigateToEditBudget() {
+        // Navegación utilizando NavController
+        val navController = findNavController()
+        navController.navigate(R.id.action_navigation_budget_to_navigation_budget_edit)
     }
 }
