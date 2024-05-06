232def16111bbd939538a581a030f6ee19065fc7
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-05-06 15:39:42 -0700

    New record fragment buttons functionality

--- app/src/main/java/angulo/javier/myfinpal/util/IconHandler.kt
+++ app/src/main/java/angulo/javier/myfinpal/util/IconHandler.kt
@@ -4,29 +4,29 @@ import android.widget.ImageView
 import angulo.javier.myfinpal.R
 
 object IconHandler {
-    fun setIcon(category: Categories, iconBgImageView: ImageView, iconImageView: ImageView) {
+    fun setIcon(category: String, iconBgImageView: ImageView, iconImageView: ImageView) {
         when (category) {
-            Categories.FOOD -> {
+            Categories.FOOD.stringValue -> {
                 iconBgImageView.setBackgroundResource(R.drawable.budget_menu_food_background)
                 iconImageView.setImageResource(R.drawable.budget_menu_food)
             }
-            Categories.SHOPPING -> {
+            Categories.SHOPPING.stringValue -> {
                 iconBgImageView.setBackgroundResource(R.drawable.budget_menu_shopping_background)
                 iconImageView.setImageResource(R.drawable.budget_menu_shopping)
             }
-            Categories.HEALTH -> {
+            Categories.HEALTH.stringValue -> {
                 iconBgImageView.setBackgroundResource(R.drawable.budget_menu_health_background)
                 iconImageView.setImageResource(R.drawable.budget_menu_health)
             }
-            Categories.ACTIVITIES -> {
+            Categories.ACTIVITIES.stringValue -> {
                 iconBgImageView.setBackgroundResource(R.drawable.budget_menu_activities_background)
                 iconImageView.setImageResource(R.drawable.budget_menu_activities)
             }
-            Categories.MEMBERSHIPS -> {
+            Categories.MEMBERSHIPS.stringValue -> {
                 iconBgImageView.setBackgroundResource(R.drawable.budget_menu_membership_background)
                 iconImageView.setImageResource(R.drawable.budget_menu_membership)
             }
-            Categories.RESTAURANTS -> {
+            Categories.RESTAURANTS.stringValue -> {
                 iconBgImageView.setBackgroundResource(R.drawable.budget_menu_restaurants_background)
                 iconImageView.setImageResource(R.drawable.budget_menu_restaurants)
             }
