8b0f360e3f83dc90940f580202a7e4b6d00a673c
Joshiva Yzair <joshivatapia@gmail.com>
2024-05-08 07:12:50 -0600

    added budget attributes

--- app/src/main/java/angulo/javier/myfinpal/domain/Budget.kt
+++ app/src/main/java/angulo/javier/myfinpal/domain/Budget.kt
@@ -1,6 +1,9 @@
 package angulo.javier.myfinpal.domain
 
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
 import java.io.Serializable
+//@Parcelize
 data class Budget(
     var budgetLimit: Double = 0.0,
     var spendBudget: Double = 0.0,
@@ -9,5 +12,11 @@ data class Budget(
     var health: Double = 0.0,
     var activities: Double = 0.0,
     var memberships: Double = 0.0,
-    var restaurants: Double = 0.0
+    var restaurants: Double = 0.0,
+    var foodLimit: Double = 0.0,
+    var shoppingLimit: Double = 0.0,
+    var healthLimit: Double = 0.0,
+    var activitiesLimit: Double = 0.0,
+    var membershipsLimit: Double = 0.0,
+    var restaurantsLimit: Double = 0.0,
 ): Serializable
