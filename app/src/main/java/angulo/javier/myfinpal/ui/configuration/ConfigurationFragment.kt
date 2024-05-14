e81ef8929792faaba93af2bb142f95c5ce5c6b8f
Idlaviv <jjavier1708@gmail.com>
2024-05-13 17:12:21 -0700

    Add logout button

--- app/src/main/java/angulo/javier/myfinpal/ui/configuration/ConfigurationFragment.kt
+++ app/src/main/java/angulo/javier/myfinpal/ui/configuration/ConfigurationFragment.kt
@@ -1,16 +1,25 @@
 package angulo.javier.myfinpal.ui.configuration
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import angulo.javier.myfinpal.LoginActivity
 import angulo.javier.myfinpal.databinding.FragmentConfigurationBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
 
 class ConfigurationFragment : Fragment() {
 
     private var _binding: FragmentConfigurationBinding? = null
+    private lateinit var auth : FirebaseAuth
+    lateinit var btn_logout: Button
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,6 +35,16 @@ class ConfigurationFragment : Fragment() {
 
         _binding = FragmentConfigurationBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        btn_logout = _binding!!.btnLogout
+
+        auth = Firebase.auth
+
+        btn_logout.setOnClickListener {
+            auth.signOut()
+            var intent = Intent(context, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         return root
     }
