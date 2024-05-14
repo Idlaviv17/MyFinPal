d60935cb5d47330f818dc502255f07eea581f6f7
Idlaviv <jjavier1708@gmail.com>
2024-05-13 20:49:46 -0700

    Retrieve the username

--- app/src/main/java/angulo/javier/myfinpal/ui/configuration/ConfigurationFragment.kt
+++ app/src/main/java/angulo/javier/myfinpal/ui/configuration/ConfigurationFragment.kt
@@ -2,24 +2,34 @@ package angulo.javier.myfinpal.ui.configuration
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import angulo.javier.myfinpal.LoginActivity
 import angulo.javier.myfinpal.databinding.FragmentConfigurationBinding
 import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 
 class ConfigurationFragment : Fragment() {
 
     private var _binding: FragmentConfigurationBinding? = null
     private lateinit var auth : FirebaseAuth
+    private lateinit var database: DatabaseReference
+    lateinit var tvUsername: TextView
     lateinit var btn_logout: Button
+    lateinit var userId: String
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,6 +45,12 @@ class ConfigurationFragment : Fragment() {
 
         _binding = FragmentConfigurationBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // Retrieve the username
+        database = Firebase.database.reference
+        tvUsername = _binding!!.tvUsernameConfiguration
+        userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+        retrieveUsername()
 
         btn_logout = _binding!!.btnLogout
 
@@ -52,5 +68,20 @@ class ConfigurationFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun retrieveUsername() {
+        val userRef = database.child("users").child(userId)
+
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val username = dataSnapshot.child("username").value.toString()
+                tvUsername.text = username
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("FirebaseError", "Error retrieving username: ${databaseError.message}")
+            }
+        })
     }
 }
